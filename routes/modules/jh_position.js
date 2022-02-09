4236187677d807840267f6ca9a21e21813beb2d6
naluwn <street.baller06@gmail.com>
2022-02-09 17:59:58 +0800

    feat: 新增顯示jh_new_position router

--- routes/modules/jh_position.js
+++ routes/modules/jh_position.js
@@ -4,12 +4,16 @@ const router = express.Router()
 const sql = require('mssql')
 const pool = require('../../config/connectPool')
 
+router.get('/new', (req, res) => {
+  res.render('jh_new_position')
+})
+
 router.get('/', (req, res) => {
   const user = res.locals.user
 	const cpyId = user.CPY_ID
   const request = new sql.Request(pool)
   const warning = []
-  
+
   request.query(`select *
   from BF_JH_POSITION
   where CPY_ID = '${cpyId}'`, (err, result) => {
@@ -21,7 +25,7 @@ router.get('/', (req, res) => {
     const positionResult = result.recordset
 
 		if(positionResult.length == 0) warning.push({message: '還未新增職缺，請拉到下方點選按鈕新增職缺!!'})
-		return res.render('position', {positionResult, warning})
+		return res.render('jh_position', {positionResult, warning})
   })
 })
 
