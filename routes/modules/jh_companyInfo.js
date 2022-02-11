93c4cad6bc7544d89a399d020ac2beab7aec7b5b
naluwn <street.baller06@gmail.com>
2022-02-11 18:19:11 +0800

    feat: 新增顯示新增公司資訊頁面

--- routes/modules/jh_companyInfo.js
+++ routes/modules/jh_companyInfo.js
@@ -4,12 +4,19 @@ const router = express.Router()
 const sql = require('mssql')
 const pool = require('../../config/connectPool')
 
+// 顯示公司新增資訊頁面
+router.get('/new', (req, res) => {
+  res.render('jh_new_cpnyInfo')
+})
+
+// 顯示公司資訊頁面
 router.get('/', (req, res) => {
   const user = res.locals.user
 	const cpyId = user.CPY_ID
   const request = new sql.Request(pool)
   const warning = []
 
+  // 抓取公司資訊
   request.query(`select a.INFO_DES, b.INFO_NAME, b.INFO_ID
   from BF_JH_CPNYINFO a
   left join BF_JH_CPNYINFO_CATEGORY b
