609beafd523ba3fd37fb95fd362db23e47d8d1b2
naluwn <street.baller06@gmail.com>
2022-02-14 09:36:13 +0800

    feat: add commit

--- modules/setDict.js
+++ modules/setDict.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 
 module.exports = {
   setPositionDict: (position_name) => {
+    // 因中文無法直接在axios直接當作網址傳送，所以需要使用encodeURI()轉成網址可以接受的格式
     position_name = encodeURI(position_name)
     const config = {
       method: 'post',
