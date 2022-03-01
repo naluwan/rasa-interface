2e7e924acdb157dbf915358f42ff8090b6d76735
naluwn <street.baller06@gmail.com>
2022-03-01 09:37:15 +0800

    feat: Mark重構換頁顯示程式碼並新增註解

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,44 +1,47 @@
 window.onload = function() {
-    if(document.querySelector("[data-func]")){
-        var pageName = document.querySelector("[data-func]").getAttribute("data-func");
-        document.documentElement.setAttribute("class",pageName);
+    var func = document.querySelector("[data-func]");
+    if(func){
+        document.documentElement.setAttribute("class",func.getAttribute("data-func"));
     };
     deleteButton();
 };
 
+//jump page
 function page(url){
     loading();
     function back(e){
-        console.log(e)
-        var html = e.data;
         var parse = new DOMParser();
-        var htmlDoc = parse.parseFromString(html,"text/html");
-        
-        if(!htmlDoc.querySelector(".setting")){
+        var html = parse.parseFromString(e.data,"text/html");
+        if(!html.querySelector(".setting")){
             history.go(0);
             return;
         };
 
-        document.querySelector(".setting").innerHTML = htmlDoc.querySelector(".setting").innerHTML;
-        history.replaceState("","",url);
+        document.querySelector(".setting").innerHTML = html.querySelector(".setting").innerHTML;
+        history.pushState("","",url);
         deleteButton();
         loadingClose();
     };
     asyncAjax(location.origin + url,back,true);
 };
 
-//ajax
+//async ajax
 function asyncAjax(url,back,async){
-    console.log(url)
     //url 路徑
-    //back function =>  var back = function(value){console.log(value);};
-    //async 多線程(可無)
+    //back is call back function
+    //async 非同步
 
     if(!back){
         console.log("missing back function!");
+        return;
     };
 
-    if(!async){
+    if(!async || !typeof(Worker)){
+
+        if(async && !typeof(Worker)){
+            console.log("Web Worker not supported to use XMLHttpRequest!");
+        };
+
         var XML = new XMLHttpRequest();
         XML.onload = function() {
             if(this.readyState == 4 && this.status == 200){
@@ -48,12 +51,6 @@ function asyncAjax(url,back,async){
         XML.open("GET", url + "?" + Math.floor(Math.random()*1000));
         XML.send();
     }else{
-
-        if(!typeof(Worker)){
-            console.log("Web Worker not support!");
-            return;
-        };
-
         var worker = new Worker("/javascript/worker.js");
         worker.postMessage(["asyncAjax",url]);
         worker.onmessage = function(e){
@@ -62,6 +59,7 @@ function asyncAjax(url,back,async){
     };
 };
 
+//loading
 function loading(){
     
     var InpBox = document.createElement("div");
@@ -80,69 +78,67 @@ function loadingClose(){
     document.querySelector("#loading").remove();
 };
 
+//delete button
 function deleteButton(){
     var obj = document.querySelector('#data-panel')
     if(obj){
-        obj.addEventListener('click', event => {
+        obj.addEventListener('click', function(event) {
             var target = event.target
-            var deleteForm = document.querySelector('#delete-form');
-            var deletePosition;
 
-            var name = target.dataset.name;
-            var category = target.dataset.category;
-            var entity = target.dataset.entity;
-            var cpnyid = target.dataset.cpnyid;
-            var table = target.dataset.table;
-            var id = target.dataset.id;
+            var dname = target.dataset.name;
+
+            if(!dname){return;};
+
+            var category   = target.dataset.category;
+            var entity     = target.dataset.entity;
+            var cpnyid     = target.dataset.cpnyid;
+            var table      = target.dataset.table;
+            var id         = target.dataset.id;
             var categoryid = target.dataset.categoryid;
             var functionid = target.dataset.functionid;
-            var action;
 
-            console.log(target)
+            var action;           
         
             if(target.id == "delete-btn"){
-                document.querySelector('#delete-position').innerText = "「" + name + "」";
+                document.querySelector('#delete-position').innerText = "「" + dname + "」";
                 action = "/" + category + "/" + entity;
             };
         
-            if(target.matches('#adminSearch-delete-btn')){
-                document.querySelector('#delete-position').innerText = '「' + cpyname + '的' + name + '」';
+            if(target.id == "adminSearch-delete-btn"){
+                document.querySelector('#delete-position').innerText = "「" + cpyname + "的" + dname + "」";
                 action = "/" + category + "/" + cpnyid + "/" + table + "/" + id;
             };
 
-            if(target.matches('#delete-function-btn')){
-                document.querySelectorAll('#delete-function').forEach(item => {
-                    item.innerText = "「" + name + "」";
+            if(target.id == "delete-function-btn"){
+                document.querySelectorAll('#delete-function').forEach(function(item){
+                    item.innerText = "「" + dname + "」";
                 });
                 action = "/" + category + "/" + id + "/" + categoryid;
             };
 
-            if(target.matches('#delete-question-btn')){
-                document.querySelectorAll('#delete-question').forEach(item => {
-                    item.innerText = "「" + name + "」";
+            if(target.id == "delete-question-btn"){
+                document.querySelectorAll('#delete-question').forEach(function(item){
+                    item.innerText = "「" + dname + "」";
                 });
-
                 action = "/" + category + "/" + id + "/" + functionid;
-
                 if(categoryid){
                     action += "/" + categoryid;
                 };
-                
             };
 
-            if(target.matches('#delete-question-admin-btn')){
-                document.querySelectorAll('#delete-question').forEach(item => {
-                    item.innerText = "「" + name + "」";
+            if(target.id == "delete-question-admin-btn"){
+                document.querySelectorAll('#delete-question').forEach(function(item){
+                    item.innerText = "「" + dname + "」";
                 })
                 action =  "/" + category + "/" + id + "/" + functionid;
             };
 
-            if(target.matches('#admin-delete-user-btn')){
-                document.querySelector('#delete-position').innerText = "「" + name + "」";
-                action =  "/" + category + "/" + name + "/" + id;
+            if(target.id == "admin-delete-user-btn"){
+                document.querySelector('#delete-position').innerText = "「" + dname + "」";
+                action =  "/" + category + "/" + dname + "/" + id;
             };
 
-            deleteForm.action = action + "?_method=DELETE";
+            document.querySelector('#delete-form').action = action + "?_method=DELETE";
         });
     };
 };
