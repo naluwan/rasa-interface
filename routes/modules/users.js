bcc47e46d9b7ecf6e55ed95e0daab7c25828b94d
naluwn <street.baller06@gmail.com>
2022-03-09 14:25:54 +0800

    feat: 重構忘記密碼及重置密碼頁面及router

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,16 +8,15 @@ const sql = require('mssql')
 const pool = require('../../config/connectPool')
 const {resetMail} = require('../../modules/sendMail')
 
-// 重置密碼
-router.put('/resetPassword/:email', (req, res) => {
+// user 重置密碼
+router.get('/resetPassword/:email/update', (req, res) => {
   const {email} = req.params
-  const {password, confirmPassword} = req.body
+  const {password, confirmPassword} = req.query
   const request = new sql.Request(pool)
 
   // 驗證密碼與確認密碼是否相符
   if(password !== confirmPassword){
-    req.flash('error', '密碼與確認密碼不符，請重新嘗試!!')
-    return res.redirect(`/users/resetPassword/${email}`)
+    return res.send({status: 'warning', message: '密碼與確認密碼不符，請重新嘗試'})
   }
 
   // 驗證帳號使否存在
@@ -30,8 +29,7 @@ router.put('/resetPassword/:email', (req, res) => {
     }
     const emailCheck = result.recordset[0]
     if(!emailCheck){
-      req.flash('error', '查無此帳號，請重新嘗試!!')
-      return res.redirect(`/users/resetPassword/${email}`)
+      return res.send({status: 'error', message: '查無此帳號，請重新嘗試'})
     }else{
       // 使用bcrypt加密並存入資料庫
       return bcrypt.genSalt(10)
@@ -45,13 +43,11 @@ router.put('/resetPassword/:email', (req, res) => {
           console.log(err)
           return
         }
-        // console.log(result)
         })
       }).then(() => {
-        req.flash('success_msg', '密碼修改成功，請使用新密碼登入!!')
         // 寄送修改密碼完成mail
-        resetMail(res, 'mail_resetPasswordDone', email, '密碼修改成功')
-        return res.redirect('/users/login')
+        resetMail(res, 'mail_resetPasswordDone', email, 'ChatBot 密碼修改成功')
+        res.send({status: 'success', message: '密碼修改成功，請使用新密碼登入'})
       }).catch(err => console.log(err))
     }
   })
@@ -65,133 +61,117 @@ router.get('/resetPassword/:email', (req, res) => {
 
 // 發送重設密碼郵件
 router.get('/sendResetMail', (req, res) => {
-  const {resetEmail} = req.query
+  const {email} = req.query
   const request = new sql.Request(pool)
+
+  if(!email) return res.send({status: 'warning', message: '請輸入E-mail'})
 
   // 驗證帳號是否存在
   request.query(`select * 
   from BOTFRONT_USERS_INFO
-  where EMAIL = '${resetEmail}'`, (err, result) => {
+  where EMAIL = '${email}'`, (err, result) => {
     if(err){
       console.log(err)
       return
     }
     const emailCheck = result.recordset[0]
     if(!emailCheck){
-      req.flash('error', '查無此帳號，請重新嘗試!!')
-      return res.redirect('/users/login')
+      res.send({status: 'error', message: '查無此帳號，請重新嘗試'})
     }else{
-      resetMail(res, 'mail_resetPassword', resetEmail, 'Botfront Interface 密碼重置')
-      req.flash('success_msg', `重設密碼郵件已經發送至${resetEmail}，請至信箱查看!`)
-      return res.redirect('/users/login')
+      resetMail(res, 'mail_resetPassword', email, 'ChatBot 密碼重置')
+      res.send({status: 'success', message: `重設密碼郵件已經發送至${email}，請至信箱查看`})
     }
   })
 })
 
-router.post('/register', (req, res) => {
-  const {cpy_no, cpy_name, email, password, confirmPassword} = req.body
-  let {isadmin} = req.body
+// user 註冊帳號
+router.get('/register/insert', (req, res) => {
+  const {cpy_no, cpy_name, email, password, confirmPassword} = req.query
 
   const request = new sql.Request(pool)
-  const errors = []
-  const warning = []
-  
-
-  // isadmin沒有要給使用者設定，故在這設預設值如果沒有收到值就給0
-  if(!isadmin) isadmin = 0
 
   // 由於0 = false，如果這邊設定檢覈的話，會一直false
-  if(!cpy_no || !cpy_name || !email || !password || !confirmPassword){
-    warning.push({message: '所有欄位都是必填的!'})
-  }
+  if(!cpy_no || !cpy_name || !email || !password || !confirmPassword) return res.send({status: 'warning', message: '所有欄位都是必填的'})
+  if(password !== confirmPassword) return res.send({status: 'warning', message: '密碼和確認密碼不相符!'})
 
-  if(password !== confirmPassword){
-    warning.push({message: '密碼和確認密碼不相符!'})
-  }
-
-  if(warning.length){
-      return res.render('register', {
-        warning,
-        cpy_no,
-        cpy_name,
-        email,
-        isadmin,
-        password,
-        confirmPassword})
-  }else{
-      request.query(`select * 
-      from BOTFRONT_USERS_INFO
-      where EMAIL = '${email}' or CPY_ID = '${cpy_no}' or CPY_NAME = '${cpy_name}'`, (err, result) => {
+  request.query(`select * 
+  from BOTFRONT_USERS_INFO
+  where EMAIL = '${email}' or CPY_ID = '${cpy_no}' or CPY_NAME = '${cpy_name}'`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    const cpnyCheck = result.recordset
+    if(cpnyCheck.length){
+      cpnyCheck.forEach(user => {
+        if(user.CPY_NAME == cpy_name) return res.send({status: 'warning', message: '此「公司名稱」已經註冊過'})
+        if(user.CPY_ID == cpy_no) return res.send({status: 'warning', message: '此「公司代號」已經註冊過'})
+        if(user.EMAIL == email) return res.send({status: 'warning', message: '此「Email」已經註冊過'})
+      })
+    }else{
+      return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => {
+        request.input('cpy_no', sql.NVarChar(30), cpy_no)
+        .input('cpy_name', sql.NVarChar(80), cpy_name)
+        .input('email', sql.NVarChar(80), email)
+        .input('password', sql.NVarChar(100), hash)
+        .query(`insert into BOTFRONT_USERS_INFO (CPY_ID, CPY_NAME, EMAIL, PASSWORD)
+        values (@cpy_no, @cpy_name, @email, @password)`, (err, result) => {
         if(err){
           console.log(err)
           return
         }
-        const user = result.recordset[0]
-        // console.log(user)
-        if(user){
-          if(user.EMAIL == email) errors.push({message: `此「Email」已經註冊過了!!`})
-
-          if(user.CPY_ID == cpy_no) errors.push({message: `此「公司代號」已經註冊過了!!`})
-
-          if(user.CPY_NAME == cpy_name) errors.push({message: `此「公司名稱」已經註冊過了!!`})
-
-          if(errors.length){
-            return res.render('register', {
-              errors,
-              cpy_no,
-              cpy_name,
-              email,
-              isadmin,
-              password,
-              confirmPassword,
-            })
-          }
-        }else{
-          return bcrypt
-          .genSalt(10)
-          .then(salt => bcrypt.hash(password, salt))
-          .then(hash => {
-            request.input('cpy_no', sql.NVarChar(30), cpy_no)
-            .input('cpy_name', sql.NVarChar(80), cpy_name)
-            .input('email', sql.NVarChar(80), email)
-            .input('isadmin', sql.Bit, parseInt(isadmin))
-            .input('password', sql.NVarChar(100), hash)
-            .query(`insert into BOTFRONT_USERS_INFO (CPY_ID, CPY_NAME, EMAIL, PASSWORD, ISADMIN)
-            values (@cpy_no, @cpy_name, @email, @password, @isadmin)`, (err, result) => {
-            if(err){
-              console.log(err)
-              return
-            }
-            
-            })
-          }).then(() => {
-            req.flash('success_msg', `帳號註冊成功!!`)
-            return res.redirect('/users/login')
-          })
-          .catch(err => console.log(err))
-        }
+        
+        })
+      }).then(() => {
+        return res.send({status: 'success', message: '帳號註冊成功'})
       })
-  }
+      .catch(err => console.log(err))
+    }
+  })
 })
 
+// 顯示user註冊頁面
 router.get('/register', (req, res) => {
     res.render('register')
 })
 
+// user登入
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/users/login',
   failureFlash: true,
 }))
 
+// 顯示user登入頁面
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
+// user登出
 router.get('/logout', (req, res) => {
-  req.logOut()
-  req.flash('success_msg', '你已經成功登出!')
-  res.redirect('/users/login')
+  const user = res.locals.user
+  if(!user) {
+    res.redirect('/users/login')
+    return 
+  }
+	const cpnyId = user.CPY_ID
+  const request = new sql.Request(pool)
+
+  request.input('logout', sql.Bit, 0)
+  .query(`update BOTFRONT_USERS_INFO
+  set ISLOGIN = @logout
+  where CPY_ID = '${cpnyId}'`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    req.logOut()
+    req.flash('success_msg', '你已經成功登出')
+    res.redirect('/users/login')
+  })
 })
 
 
