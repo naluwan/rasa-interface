05a684fda640f6834dca02a28a5c070e73d300ac
naluwn <street.baller06@gmail.com>
2022-03-09 09:50:05 +0800

    feat: admin操作sql，新增、更新、刪除並新增註解

--- modules/useSql.js
+++ modules/useSql.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // 徵厲害 user 新增資訊
   insertDes: (request, sql, res, data, fsFunc) => {
     // 設定sql table、欄位名稱
     const table = `BF_JH_${data.category.toUpperCase()}`
@@ -60,17 +61,17 @@ module.exports = {
             return
           }
 
-            // 寫檔及寫入dict
-          switch(data.category.toUpperCase()){
-            case 'CPNYINFO':
+          // 寫檔及寫入dict
+          switch(data.category){
+            case 'cpnyinfo':
               fsFunc.fsJhWriteInfo(data.cnName, data.entity_name, request)
               fsFunc.setInfoDict(data.cnName)
               break
-            case 'POSITION':
+            case 'position':
               fsFunc.fsJhWritePosition(data.cnName, data.entity_name, request)
               fsFunc.setPositionDict(data.cnName)
               break
-            case 'SUBSIDY':
+            case 'subsidy':
               fsFunc.fsWriteSubsidy(data.cnName, data.entity_name, request)
               fsFunc.setInfoDict(data.cnName)
               break
@@ -79,7 +80,6 @@ module.exports = {
               fsFunc.setInfoDict(data.cnName)
               break
           }
-
 
           // 取得剛新增的類別id
           request.query(`select ${category_id} as id
@@ -110,7 +110,8 @@ module.exports = {
       }
     })
   },
-  editDes: (request, sql, res, data) => {
+  // 徵厲害 user 編輯資訊
+  updateDes: (request, sql, res, data) => {
     // 設定sql table、欄位名稱
     const table = `BF_JH_${data.category.toUpperCase()}`
     const table_category = `BF_JH_${data.category.toUpperCase()}_CATEGORY`
@@ -146,6 +147,7 @@ module.exports = {
       }
     })
   },
+  // 徵厲害 user 刪除資訊
   deleteDes: (request, res, data) => {
     // 設定sql table、欄位名稱
     const table = `BF_JH_${data.category.toUpperCase()}`
@@ -173,6 +175,168 @@ module.exports = {
           }
           res.send({status: 'success', message: '刪除資訊成功'})
         })
+      }
+    })
+  },
+  // 徵厲害 admin 編輯類別
+  updateCategory: (request, sql, res, data, fsFunc) => {
+    // 設定sql table、欄位名稱
+    const table = `BF_JH_${data.category.toUpperCase()}`
+    const table_category = `BF_JH_${data.category.toUpperCase()}_CATEGORY`
+    const category_id = `${data.category.toUpperCase()}_ID`
+    const category_name = `${data.category.toUpperCase()}_NAME`
+    const category_des = `${data.category.toUpperCase()}_DES`
+
+    request.query(`select * 
+    from ${table_category}
+    where ${category_id} = ${data.infoId}
+    and ENTITY_NAME = '${data.entity}'`, (err, result) => {
+      if(err){
+        console.log(err)
+        return
+      }
+      
+      try {
+        const infoCheck = result.recordset[0][`${category_name}`]
+
+        request.input('cnName', sql.NVarChar(200), decodeURI(data.cnName))
+        .input('entity_name', sql.NVarChar(200), data.entity_name)
+        .query(`update ${table_category}
+        set ${category_name} = @cnName, ENTITY_NAME = @entity_name
+        where ${category_id} = ${data.infoId}`, (err, result) => {
+          if(err){
+            console.log(err)
+            return
+          }
+          let intent = ''
+          switch (data.category) {
+            case 'position':
+              intent = '職缺'
+              fsFunc.setPositionDict(decodeURI(data.cnName))
+              break;
+            case 'leave':
+              intent = '問假別資訊'
+              fsFunc.setInfoDict(decodeURI(data.cnName))
+              break;
+            case 'subsidy':
+              intent = '問補助資訊'
+              fsFunc.setInfoDict(decodeURI(data.cnName))
+              break;
+            default:
+              intent = '問公司資訊'
+              fsFunc.setInfoDict(decodeURI(data.cnName))
+              break;
+          }
+          fsFunc.fsUpdateCategoryNlu(infoCheck, intent, decodeURI(data.cnName), data.entity_name, request)
+          res.send({status: 'success', message: '更新資訊內容成功'})
+        })
+      } catch (error) {
+        res.send({status: 'error', message: '查無此資訊類別，請重新嘗試'})
+      }
+    })
+  },
+  // 徵厲害 admin 新增類別
+  insertCategory: (request, sql, res, data, fsFunc) => {
+    // 設定sql table、欄位名稱
+    const table = `BF_JH_${data.category.toUpperCase()}`
+    const table_category = `BF_JH_${data.category.toUpperCase()}_CATEGORY`
+    const category_id = `${data.category.toUpperCase()}_ID`
+    const category_name = `${data.category.toUpperCase()}_NAME`
+    const category_des = `${data.category.toUpperCase()}_DES`
+
+    request.query(`select * 
+    from ${table_category}
+    where ${category_name} = '${data.cnName}'`, (err, result) => {
+      if(err){
+        console.log(err)
+        return
+      }
+
+      const infoCheck = result.recordset[0]
+      if(infoCheck){
+        return res.send({status: 'warning', message: '類別重複，請重新嘗試'})
+      }else{
+        request.input('cnName', sql.NVarChar(200), decodeURI(data.cnName))
+        .input('entity_name', sql.NVarChar(200), data.entity_name)
+        .query(`insert into ${table_category} (${category_name}, ENTITY_NAME) 
+        values (@cnName, @entity_name)`, (err, result) => {
+          if(err){
+            console.log(err)
+            return
+          }
+
+          // 寫檔及寫入dict
+          switch(data.category){
+            case 'cpnyinfo':
+              fsFunc.fsJhWriteInfo(data.cnName, data.entity_name, request)
+              fsFunc.setInfoDict(data.cnName)
+              break
+            case 'position':
+              fsFunc.fsJhWritePosition(data.cnName, data.entity_name, request)
+              fsFunc.setPositionDict(data.cnName)
+              break
+            case 'subsidy':
+              fsFunc.fsWriteSubsidy(data.cnName, data.entity_name, request)
+              fsFunc.setInfoDict(data.cnName)
+              break
+            default:
+              fsFunc.fsWriteLeave(data.cnName, data.entity_name, request)
+              fsFunc.setInfoDict(data.cnName)
+              break
+          }
+
+          res.send({status: 'success', message: '新增類別成功'})
+        })
+      }
+    })
+  },
+  // 徵厲害 admin 刪除類別
+  deleteCategory: (request, res, data, fsFunc) => {
+    // 設定sql table、欄位名稱
+    const table = `BF_JH_${data.category.toUpperCase()}`
+    const table_category = `BF_JH_${data.category.toUpperCase()}_CATEGORY`
+    const category_id = `${data.category.toUpperCase()}_ID`
+    const category_name = `${data.category.toUpperCase()}_NAME`
+    const category_des = `${data.category.toUpperCase()}_DES`
+
+    request.query(`select * 
+    from ${table_category}
+    where ${category_id} = ${data.infoId}`, (err, result) => {
+      if(err){
+        console.log(err)
+        return
+      }
+      
+      try {
+        const infoCheck = result.recordset[0][`${category_name}`]
+        request.query(`delete from ${table_category} 
+        where ${category_id} = ${data.infoId}`, (err, result) => {
+          if(err){
+            console.log(err)
+            return
+          }
+
+          let intent = ''
+          // 寫檔及寫入dict
+          switch(data.category){
+            case 'cpnyinfo':
+              intent = '問公司資訊'
+              break
+            case 'position':
+              intent = '職缺'
+              break
+            case 'subsidy':
+              intent = '問補助資訊'
+              break
+            default:
+              intent = '問假別資訊'
+              break
+          }
+          fsFunc.fsJhDeleteNlu(infoCheck, intent, request)
+          res.send({status: 'success', message: '刪除類別成功'})
+        })
+      } catch (error) {
+        return res.send({status: 'error', message: '查無此類別，請重新嘗試'})
       }
     })
   }
