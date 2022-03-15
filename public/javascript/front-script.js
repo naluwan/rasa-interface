56890fc68daf18c2d8e7d938bd2f679f1e37266b
naluwn <street.baller06@gmail.com>
2022-03-15 17:49:46 +0800

    feat: 更新客服訓練按鈕事件

--- public/javascript/front-script.js
+++ public/javascript/front-script.js
@@ -2,24 +2,16 @@ const messageBlock = document.querySelector('#messageBlock')
 const csTrainBtn = document.querySelector('.cs-train-btn')
 const jhTrainBtn = document.querySelector('.jh-train-btn')
 const questionDes = document.querySelector('.question-description')
+const adminNewSearch = document.querySelector('.admin_new_search')
 
 if(csTrainBtn){
 	csTrainBtn.addEventListener('click', e => {
 		const target = e.target
 		if(target.matches('.cs-train-btn')){
-			console.log('訓練中...')
 			csTrainBtn.setAttribute('disabled', '')
 			csTrainBtn.innerHTML = '<i class="fas fa-spinner fast-spin fa-2x"></i>'
-			messageBlock.innerHTML = `
-			<div class="alert alert-warning alert-dismissible fade show" role="alert">
-				訓練中.....
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-			`
 			
-			fetch('http://localhost:3030/train/cs/trainingData')
+			fetch('http://192.168.11.80:3030/train/cs/trainingData')
 			.then(response => {
 				return response.json()
 			})
@@ -33,24 +25,17 @@ if(csTrainBtn){
 					mode: 'no-cors',
 				})
 				.then(response => {
-					fetch('http://localhost:3030/train/trainingComplete')
+					fetch('http://192.168.11.80:3030/train/trainingComplete')
 					.then(response => {
 						return response.json()
 					})
 					.then(result => {
-						console.log(`功能訓練結果：` + result[1].functions)
-						console.log(`問答資訊訓練結果：` + result[0].question)
-						console.log('訓練完成!!')
 						csTrainBtn.removeAttribute('disabled')
-						csTrainBtn.innerText = 'Train'
-						messageBlock.innerHTML = `
-						<div class="alert alert-success alert-dismissible fade show" role="alert">
-							訓練完成!!
-							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-								<span aria-hidden="true">&times;</span>
-							</button>
-						</div>
-						`
+						csTrainBtn.innerText = '執行訓練'
+
+						// 訓練完成提示框
+						var html = "<h1><div class='sa-icon success'><span></span></div>訓練完成</h1>";
+						Method.common.showBox(html, 'message');
 					})
 					.catch(err => console.log(err))
 				})
@@ -121,6 +106,27 @@ if(questionDes){
 	})
 }
 
+// if(adminNewSearch){
+// 	adminNewSearch.addEventListener('click', e => {
+// 		const target = e.target
+// 		console.log(target)
+
+// 		let cpnyId = ''
+// 		let category = ''
+// 		if(target.matches('.cpnyId')){
+// 			console.log(target)
+// 			cpnyId = target.value
+// 		}
+
+// 		if(target.matches('.category')){
+// 			console.log(target)
+
+// 			category = target.value
+// 		}
+
+// 		document.querySelector('#cancel').onclick = `Method.common.page('/admin_search/filter?companyFilter=${cpnyId}&tableFilter=${category}&search=')`
+// 	})
+// }
 
 
 
