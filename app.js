4f1ec4bbf87949a675c619a1637eda96ec254b5d
naluwn <street.baller06@gmail.com>
2022-07-28 17:50:01 +0800

    feat: 新增express.json()

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(session({
   saveUninitialized: true
 }))
 app.use(express.static('public'))
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
 
