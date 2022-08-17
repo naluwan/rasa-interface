2055dddedc2f949fda9031112a18f0c283a0a094
naluwn <street.baller06@gmail.com>
2022-08-17 15:47:32 +0800

    feat: 套用JWT Token套件，新增驗證JWT Token middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,8 @@
+const jwt = require('jsonwebtoken')
+const SECRET = 'thisisthenewjwttoken'
+const sql = require('mssql')
+const pool = require('../config/connectPool')
+
 module.exports = {
   authenticator: (req, res, next) => {
     if (req.isAuthenticated()) {
@@ -13,5 +18,40 @@ module.exports = {
     }
     req.flash('warning_msg', '權限不足')
     res.redirect('/')
+  },
+  // JWT 驗證
+  auth: (req, res, next) => {
+    const request = new sql.Request(pool)
+    try{
+      const token = req.header('Authorization').replace('Bearer ', '')
+      const decoded = jwt.verify(token, SECRET, {expiresIn: '10000'})
+
+      request.query(`select CPY_ID, CPY_NAME, EMAIL, ISADMIN, TOKENS
+      from BOTFRONT_USERS_INFO
+      where CPY_ID = '${decoded.id}'`, (err, result) => {
+        if(err){
+          console.log(err)
+          return
+        }
+
+        const user = result.recordset[0]
+        if(!user) {throw new Error()}
+
+        const tokens = JSON.parse(user.TOKENS)
+        const isCurrentToken = tokens.some(item => item.token === token)
+        
+        if(isCurrentToken){
+          delete user.TOKENS
+          req.token = token
+          req.user = user
+          next()
+        }else{
+          res.status(401).send({status: 'error', message:'無效Token'})
+        }
+      })
+    }catch(err){
+      res.status(401).send({status: 'error', message:'資料錯誤，請重新嘗試'})
+    }
   }
 }
+
